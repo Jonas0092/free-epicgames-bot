ffca6c284f5333c2368265258cd099faf8c035a0
Jonas0092 <accounts@jonas0092.de>
2026-03-12 18:26:08 +0100

    Implemented checking loop

--- src/main.py
+++ src/main.py
@@ -22,22 +22,36 @@ def main() -> None:
     event_loop = asyncio.new_event_loop()
     asyncio.set_event_loop(event_loop)
 
-    Thread(target=send_message, args=(bot, event_loop)).start()
+    check_interval = int(os.getenv("FREE_GAMES_CHECK_INTERVAL"))
+    if not check_interval or check_interval <= 0:
+        check_interval = 3600
+
+    Thread(target=check_free_games_loop, args=(bot, check_interval, event_loop)).start()
 
     token: str = os.getenv("BOT_TOKEN")
 
     bot.start(token, event_loop)
 
 
-def send_message(bot: FluxerBot, event_loop: AbstractEventLoop) -> None:
+async def check_free_games_loop_async(bot: FluxerBot, check_interval: int) -> None:
     channel_id = int(os.getenv("CHANNEL_ID"))
-    message: str = build_free_games_message()
-    asyncio.run_coroutine_threadsafe(bot.send_message(channel_id, message), event_loop)
+
+    while True:
+        new_free_games: list[dict] = epicgames.get_new_free_games()
+        if len(new_free_games) > 0:
+            message: str = build_free_games_message(new_free_games)
+            await bot.send_message(channel_id, message)
+
+        await asyncio.sleep(check_interval)
 
 
-def build_free_games_message() -> str:
+def check_free_games_loop(bot: FluxerBot, check_interval: int, event_loop: AbstractEventLoop) -> None:
+    asyncio.run_coroutine_threadsafe(check_free_games_loop_async(bot, check_interval), event_loop)
+
+
+def build_free_games_message(games: list[dict]) -> str:
     message = "### Free Games:\n"
-    for game in epicgames.get_new_free_games():
+    for game in games:
         message += f"**{game['title']}**\n{game['description']}\n\n"
     return message.strip()
 
